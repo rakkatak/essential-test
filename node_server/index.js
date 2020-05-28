5bd576651a7b06b42a366cf787253ab92fc4e95c
rakkatak <anita@rakkatak.com>
2020-05-27 23:55:13 -0400

    eliminating redundant code

--- node_server/index.js
+++ node_server/index.js
@@ -11,9 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //form-urlencoded
 
-// for parsing multipart/form-data
-app.use(upload.array());
-app.use(express.static('public'));
+// to elimiate any cross origin errors
 app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
